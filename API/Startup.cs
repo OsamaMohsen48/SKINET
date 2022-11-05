848de10a46f1bcf8447663d81df07629c210751e
OsamaMohsen48 <osamamohsen28@gmail.com>
2022-11-05 15:50:21 +0200

    End of Section5 without Swg

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
+using API.Errors;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -37,15 +39,32 @@ namespace API
             services.AddDbContext<StoreContext>(option =>
             { option.UseSqlServer(@"Data Source=.;Initial Catalog=SKINET;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); }
             );
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = ActionContext =>
+                  {
+                      var errors = ActionContext.ModelState
+                      .Where(e => e.Value.Errors.Count > 0)
+                      .SelectMany(x => x.Value.Errors)
+                      .Select(x => x.ErrorMessage).ToList();
+
+                      var errorResponse = new ApiValidationErrorResponse { Errors = errors };
+
+                      return new BadRequestObjectResult(errorResponse);
+                  };
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseRouting();
             app.UseStaticFiles();
